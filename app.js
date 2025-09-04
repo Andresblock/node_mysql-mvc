66e7242a2d743710a8ba73c9169900a8c554fe25
Anderson Andrés Gómez Cerón <anderson.gomez@bebolder.local>
2025-09-04 10:46:49 -0500

    actualizacion6

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use(express.json())
 const usuario = require('./src/routes/hwusuario.route')
 const catalogo = require('./src/routes/hwcatalogo.route')
 const login = require('./src/routes/hwlogin.route')
+const collection = require('./src/routes/hwcollection.route')
 
 // Rutas:
 app.use('/api/v1/usuario',usuario)
 app.use('/api/v1/catalogo',catalogo)
 app.use('/api/v1/loginhw',login)
+app.use('/api/v1/coleccion',collection)
 
 
 app.get('/', (req,res)=>{
